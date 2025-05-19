fd9caf99e7697e46674d334ae97c9fb812998ba6
taylanmayckon <eusoutaylann@gmail.com>
2025-05-19 14:39:44 -0300

    Add: Libs do projeto

--- AlertaEnchentes.c
+++ AlertaEnchentes.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Includes do FreeRTOS
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+
+// Includes para configuração dos periféricos da BitDogLab
+#include "led_matrix.h"
+#include "ssd1306.h"
+#include "hardware/pwm.h"
+#include "hardware/i2c.h"
+#include "font.h"
+#include "hardware/adc.h"   
+     
+
 
 
 int main(){
